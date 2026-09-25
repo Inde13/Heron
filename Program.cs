b6676a824964c563ec9b12c343ef40dfa4c6d582
Inde13 <mahfariah13@gmail.com>
2026-09-25 20:20:55 -0300

    Adiciona atividade Heron finalizada

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+
+double ladoA, ladoB, ladoC, s, area; // s = semiperímetro
+
+Console.WriteLine("-= Calculadora de Heron =-\n");
+
+Console.WriteLine("Insira as medidas do triângulo (em centímetros):");
+
+Console.Write("Lado A: ");
+ladoA = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Lado B: ");
+ladoB = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Lado C: ");
+ladoC = Convert.ToDouble(Console.ReadLine());
+
+s = (ladoA + ladoB + ladoC) / 2;
+
+area = Math.Sqrt(s * (s - ladoA) * (s - ladoB) * (s - ladoC));
+
+Console.WriteLine("\n-= Resultado =-\n");
+
+Console.Write("Área do triângulo: ");
+
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine($"{area}cm");
+
+Console.ResetColor();
